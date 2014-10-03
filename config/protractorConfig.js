e81c360f178ad16e390084fb47187777de29213b
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-10-03 17:11:41 -0300

    se agrega soporte de test en sourceLab

--- config/protractorConfig.js
+++ config/protractorConfig.js
@@ -1,10 +1,56 @@
 // conf.js
-exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
 
+extend = require('node.extend');
+
+var environment = process.env.NODE_ENV || "dev";
+
+//console.log(environment);
+
+var genericConfig = {
     suites: {
-        login: ['../test/model/loginPage.js','../test/spec.js'],
+        login: ['../test/model/loginPage.js', '../test/spec.js'],
         student: ['../test/studentSpec.js'],
         proffesor: ['../test/proffesorSpec.js']
     }
-}	
+};
+
+var genericCapability = {
+    'name': process.env.CI_MESSAGE || 'Ad hoc message',
+    'build': process.env.CI_BUILD_NUMBER + ' (' + (process.env.CI_COMMIT_ID || "No comments.").substring(0, 7) + ')',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+};
+
+var configurationByEnvironment = {
+    test: extend({
+        sauceUser: 'MEugeC',
+        sauceKey: '360f20e6-cbab-4cdf-914f-cfd7b12ee329',
+        multiCapabilities: [
+            extend({
+                browserName:'chrome'
+            },genericCapability),
+            extend({
+                browserName:'firefox'
+            },genericCapability),
+            extend({
+                browserName:'internet explorer',
+                platform: 'windows 8.1',
+                version: 11
+            },genericCapability)
+
+        ]
+    }, genericConfig),
+
+    dev: extend({
+        seleniumAddress: 'http://localhost:4444/wd/hub',
+        capabilities: {
+            browserName: 'chrome',
+            name: "developmentBuild",
+            build: "NA"
+        }
+    }, genericConfig)
+
+};
+
+exports.config = configurationByEnvironment[environment];
+
+//console.log(exports.config);
