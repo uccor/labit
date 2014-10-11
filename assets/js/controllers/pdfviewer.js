adee258c6643afec04c341215d1943472349a992
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-10-11 20:39:37 -0300

    * Professor page divided in proffesor managment and professor screen * Changes to make student page work again * Added routes to ProfessorManagment to allow diferent templates

--- assets/js/controllers/pdfviewer.js
+++ assets/js/controllers/pdfviewer.js
@@ -4,27 +4,63 @@
 
 app.controller('pdfViewer', ['$scope', '$rootScope', "$sailsBind", function ($scope, $rootScope, $sailsBind) {
 
-    $scope.pdf = {}
+    $scope.pdf = {};
+    $scope.pdfUrl;
 
     $scope.pageNum = 0;
     $scope.pageRendering = false;
     $scope.pageNumPending = null;
     $scope.pageTotal = 0;
+    $scope.navigation = false;
 
-    $scope.getpdf = function(file, pag){
+    $scope.getpdf = function (file, pag, navigation) {
         pag = typeof pag !== 'undefined' ? pag : 1;
+        navigation = typeof navigation !== 'undefined' ? navigation : true;
+        if (file == '') {
+            $scope.pageNum = 0;
+            $scope.pageTotal = 0;
+            $scope.pdfUrl = '';
+            $scope.navigation = false;
 
-        $scope.pageNum = 1;
-        PDFJS.getDocument(file).then(function(pdf){
-        $scope.pdf = pdf;
-        $scope.changePage(pag);
-        });
+            // Limpio el canvas
+            var canvas = document.getElementById('contentShareCanvas');
+            var ctx = canvas.getContext('2d');
+                // Store the current transformation matrix
+            ctx.save();
+
+            // Use the identity matrix while clearing the canvas
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Restore the transform
+                            ctx.restore();
+
+        } else {
+            $scope.navigation = navigation;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+            if (file != $scope.pdfUrl) {
+                $scope.pdfUrl = file;
+                PDFJS.getDocument(file).then(function (pdf) {
+                    $scope.pdf = pdf;
+                    $scope.pageTotal = pdf.numPages;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                    $scope.changePage(pag);
+                });
+            } else if ($scope.pageNum != pag) {
+                $scope.changePage(pag);
+            }
+        }
     }
 
 
     $scope.changePage = function (pagina) {
 
         $scope.pageTotal = $scope.pdf.numPages;
+        $scope.pageNum = pagina;
 
         if ($scope.pageRendering) {
             $scope.pageNumPending = pagina;
@@ -38,7 +74,7 @@ app.controller('pdfViewer', ['$scope', '$rootScope', "$sailsBind", function ($sc
             var canvas = document.getElementById('contentShareCanvas');
             var context = canvas.getContext('2d');
             var scale = 1;
-            canvas.width  = canvas.offsetWidth;
+            canvas.width = canvas.offsetWidth;
 
             var viewport = page.getViewport(canvas.width / page.getViewport(1.0).width);
 
@@ -60,36 +96,17 @@ app.controller('pdfViewer', ['$scope', '$rootScope', "$sailsBind", function ($sc
                     $scope.changePage($scope.pageNumPending);
                     $scope.pageNumPending = null;
                 }
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
             });
         });
     };
 
-    $scope.$on('pdfChangePage', function(event, args) {
-        $scope.changePage(args);
-        $scope.pageNum = args;
+    $scope.$on('pdfChange', function (event, args) {
+        $scope.getpdf(args.file, args.pag, args.nav);
     });
 
-    $scope.$on('pdfChange', function(event, args) {
-        $scope.getpdf(args.file, args.pag);
-    });
-
-    $scope.prevPage = function() {
-        if ($scope.pageNum <= 1) {
-            return;
-        }
-        $scope.pageNum--;
-        $scope.changePage($scope.pageNum);
-        $rootScope.$broadcast('pdfPageChanged', $scope.pageNum);
-    };
-
-    $scope.nextPage = function() {
-        if ($scope.pageNum >= $scope.pageTotal) {
-            return;
-        }
-        $scope.pageNum++;
-        $scope.changePage($scope.pageNum);
-        $rootScope.$broadcast('pdfPageChanged', $scope.pageNum);
-    };
 }]);
 
 
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -21,19 +21,34 @@ module.exports.bootstrap = function (cb) {
     sails.services.passport.loadStrategies();
 
     Pdf.find({id: 'PDF1'}).exec(function (err, pdf) {
-        if(pdf.length > 0) { return  }
-            Pdf.create([
-                {id: 'PDF1', nombre: 'pdf1_example', ruta: '/pdf/pdf1.pdf'},
-                {id: 'PDF2', nombre: 'pdf2_example', ruta: '/pdf/pdf2.pdf'},
-                {id: 'PDF3', nombre: 'pdf3_example', ruta: '/pdf/pdf3.pdf'},
-                {id: '4', nombre: 'DerechoPenal', ruta: '/pdf/DerechoPenal.pdf'},
-                {id: '5', nombre: 'Scrum', ruta: '/pdf/Scrum.pdf'}
-            ]).exec(afterPdf);
+        if (pdf.length > 0) {
+            return
+        }
+        Pdf.create([
 
-            Live_class_student.create([
-                {id: 'CLASS1', pdf_activo: 'false', pdf_ruta: '', pdf_numeroPagina: '2'},
-                {id: 'CLASS2', pdf_activo: 'false', pdf_ruta: '', pdf_numeroPagina: '2'}
-            ]).exec(afterPdf);
+            {id: 'PDF1', nombre: 'pdf1_example', ruta: '/pdf/pdf1.pdf'},
+            {id: 'PDF2', nombre: 'pdf2_example', ruta: '/pdf/pdf2.pdf'},
+            {id: 'PDF3', nombre: 'pdf3_example', ruta: '/pdf/pdf3.pdf'},
+            {id: '4', nombre: 'DerechoPenal', ruta: '/pdf/DerechoPenal.pdf'},
+            {id: '5', nombre: 'Scrum', ruta: '/pdf/Scrum.pdf'}
+        ]).exec(afterPdf);
+
+        Live_class_student.create([
+            {  id: 'CLASS1', pdf_sharing: 'false',
+                pdf_synchronize: false,
+                pdf_allowNavigation: false,
+                pdf_url: '',
+                pdf_studentPageNumber: 0,
+                pdf_screenPageNumber: 0,
+            },
+            {  id: 'CLASS2', pdf_sharing: 'false',
+                pdf_synchronize: false,
+                pdf_allowNavigation: false,
+                pdf_url: '',
+                pdf_studentPageNumber: 0,
+                pdf_screenPageNumber: 0,
+            }
+        ]).exec(afterPdf);
     });
     cb();
 };
