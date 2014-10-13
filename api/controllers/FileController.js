060624f840a57918c1c642c77c9f1a9bbbb7f90a
Agustina Edme Aliciardi <edmealiciardi@gmail.com>
2014-10-12 22:38:10 -0300

    Se creo el Formulario para que el profesor suba el PDF y el FileController.js  (se utilizo el $npm install skipper y Sails generate api file)

---
+++ api/controllers/FileController.js
@@ -0,0 +1,32 @@
+/**
+ * FileController
+ *
+ * @description :: Server-side logic for managing files
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+        upload: function  (req, res) {
+            if(req.method === 'GET')
+                return res.json({'status':'GET not allowed'});                       // Call to /upload via GET is error
+
+            var uploadFile = req.file('uploadFile');
+            //uploadFile.dirname = ('/pdf');
+            console.log(uploadFile);
+
+            uploadFile.upload({
+                dirname: './assets/pdf'
+            //adapter: require('skipper-gridfs'),                                       //Para subir a una base de datos necesita configuracion adicional
+            //uri: 'mongodb://[username:password@]host1[:port1][/[database[.bucket]]'
+            //e.g. mongodb://jimmy@j1mtr0n1xx@mongo.jimmy.com:27017/coolapp.avatar_uploads
+            },function onUploadComplete (err, files) {                // Files will be uploaded to .tmp/uploads
+
+                if (err) return res.serverError(err);                              // IF ERROR Return and send 500 error with error
+
+                console.log(files);
+                res.json({status:200,file:files});
+            });
+        }
+    };
+
