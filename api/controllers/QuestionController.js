af3aa9a6ff78194ca576635f08eff3b4f7f30cf8
stefanoantonel <stefanoantonel@gmail.com>
2014-10-14 10:22:28 -0300

    student can't answer the same question

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -8,29 +8,24 @@
 module.exports = {
 	getVisible : function (req, res) {
 		
-		// var questionId = req.param('id');
-
-		Question.find({visible : true})
-		.exec(function(err, ques) {
-			
-			// console.log('questionId', questionId);
-			// console.log('answerLength: ',ans.length );
-			// var answers = [];
-			// ans.forEach(function(an) {
-			// 	a = {
-			// 		"user": an.user.username,
-			// 		"answer": an.question.answers[an.userAnswer]
-			// 	};	
-			// 	answers.push(a);
-			// });
-
-			// var quest = Question.findOne({id:questionId}).exec(function(err, ques) {
-			// 	res.json({"responsesArray": answers});
-			// });	
-			res.json({"questions": ques});
+		Answer.find({
+			user: req.user.id,
+		})
+		.exec(function(err, ans) {
+			// console.log('ans from user: ',ans);
+			quesAnswered = [];
+			ans.forEach(function(an) {
+				quesAnswered.push(an.question);
+			});
+			// console.log('answered: ',quesAnswered);
+			Question.find({
+				visible : true,
+				id : {"!" : quesAnswered}
+			})
+			.exec(function(err, ques) {	
+				res.json({"questions": ques});
+			});
 		});
-		
-	
 	}
 	
 
