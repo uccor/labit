adc513daff73d7fffd4c8b3618c1da2f4dfc03d5
GuilleDC <guilledc2000@hotmail.com>
2014-10-16 14:00:42 -0300

    JS of professorScreen. Tested. Location not working, mocked up.

--- assets/js/controllers/professorScreen.js
+++ assets/js/controllers/professorScreen.js
@@ -1,32 +1,58 @@
 /**
  * Created by martin on 11/10/14.
  */
-
-app.controller('professorFileShare', ['$scope', '$rootScope', "$sailsBind", function ($scope, $rootScope, $sailsBind) {
-
-    $scope.live_class_students = {};
-    $scope.warning = {
-        msg: '',
-        class: 'hidden'
+app.controller('professorScreen', ['$scope', '$rootScope', "$sailsBind", function ($scope, $rootScope, $sailsBind) {
+    $scope.idClase = '';
+    $scope.actClass = {}
+    $scope.paginaActualPDF = 0;
+     // Subscription
+    var x_act_class="CLASS1"; //$location.hash();
+     $scope.subscribe_to_class = function () {
+         io.socket.get('/api/live_class_student' + x_act_class, function messageReceived(jsonObject) {
+                 $scope.idClase = jsonObject.id;
+                 $scope.actClass = jsonObject;
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+                $scope.updatePDF();
+            }
+        );
     };
+    $scope.subscribe_to_class();
+    // Listen to incoming Updates from Live_class_student we just suscribed to
+    io.socket.on('live_class_student', function messageReceived(jsonObject) {
 
-    $scope.share = function (id, route, nombre) {
-
-        $scope.pdf_id = id;
-        $scope.pdf_nombre = nombre;
-        $scope.pdf_route = route;
-
-        if ($scope.$parent.id_class_to_share != '' && typeof $scope.$parent.id_class_to_share !== 'undefined') {
-            $rootScope.$broadcast('pdfChange', {file: route, pag: 1, name: nombre});
-            $scope.warning.msg = "";
-            $scope.warning.class = 'hidden';
+        if (jsonObject.id == $scope.idClase) {
+            switch (jsonObject.verb) {
+                case 'updated':
+                    for (var k in jsonObject.data) {
+                        $scope.actClass[k] = jsonObject.data[k];
+                    }
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                    $scope.updatePDF();
+                default:
+                break;
+            }
         }
-        else {
-            $scope.warning.msg = "Seleccione una clase";
-            $scope.warning.class = '';
+    });
+
+    $scope.updatePDF = function(){
+        if ($scope.actClass.pdf_sharing) {
+            $rootScope.$broadcast('pdfChange',
+                { file: $scope.actClass.pdf_url,
+                    pag: $scope.actClass.pdf_studentPageNumber
+                }
+            );
+        } else {
+            $rootScope.$broadcast('pdfChange',
+                { file: '',
+                    pag: 0,
+                    nav: false
+                }
+            );
         }
     };
-
-    $sailsBind.bind("api/pdf", $scope);
 
 }]);
