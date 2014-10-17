27c5c61f298898380ceacad1314e3c52b5e1504d
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-10-17 01:47:18 -0300

    Fixed merge error (missing , in array and layout.ejs JS lib)

--- config/protractorConfig.js
+++ config/protractorConfig.js
@@ -10,7 +10,7 @@ var genericConfig = {
     suites: {
         login: ['../test/loginSpec.js'],
         //     register: ['../test/registerSpec.js'],
-        question: ['../test/questionSpec.js']
+        question: ['../test/questionSpec.js'],
         student: ['../test/studentSpec.js'],
         professorMFileShare: ['../test/professorMFileShareSpec.js'],
         professorUploadSpec: ['../test/professorUploadSpec.js']
