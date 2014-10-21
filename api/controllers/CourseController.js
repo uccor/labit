ea024664b851306b92770e57c1b69816eaacd79d
stefanoantonel <stefanoantonel@gmail.com>
2014-10-20 21:59:13 -0300

    Creacion del modelo Course

---
+++ api/controllers/CourseController.js
@@ -0,0 +1,11 @@
+/**
+ * CourseController
+ *
+ * @description :: Server-side logic for managing Courses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
