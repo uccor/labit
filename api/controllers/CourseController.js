5a0582640857b6f0f30878d3cb4026f48dd759cc
fabriciocollino <fabricio.collino@gmail.com>
2014-10-23 02:38:58 -0300

    course controller for sails-bind with user auth

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -5,16 +5,17 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 /*
-    Routes:
-        'post /api/newCourse':  'CourseController.create_course',
-        'get  /api/myCourses':  'CourseController.get_my_courses',
-        'put  /api/editCourse': 'CourseController.set_my_course'
+ //Routes last sprint
+     'put  /api/course/create':      'CourseController.create',
+     'get  /api/course':             'CourseController.get_my_courses',
+     'put  /api/course/update':      'CourseController.update',
+     'delete  /api/course/destroy':  'CourseController.update'
 */
 
 
 module.exports = {
 
-    create_course : function (req, res) {
+    create : function (req, res) {
 
         var courseName  =   req.param('name');
         var userID      =   req.session.passport.user;
@@ -36,18 +37,24 @@ module.exports = {
                 return;
             }
 
-            Course.create({name:courseName}).exec(function afterCreated(err,createdCourse){
+            Course.create({name:courseName}).exec(function afterCreated(err,course){
                 if(err){
                     res.send(err);
                     return;
                 }
 
                 // Asocio la clase nueva al usuario que lo solicito
-                createdCourse.users.add(userID);
-                createdCourse.save();
+                course.users.add(userID);
+                course.save();
                 res.send('Nueva clase creada correctamente');
+
+                if (req.isSocket){
+                    Course.publishCreate({id:course.id,name:course.name});
+                }
             });
         });
+
+
     },
 
     get_my_courses : function (req, res) {
@@ -72,32 +79,94 @@ module.exports = {
             //console.log(userCourses);
             res.send(userCourses);
         });
+
     },
 
 
 
-    set_my_course : function (req, res) {
-    /*
+    update : function (req, res) {
 
-        var userID = req.session.passport.user;
+        var userID          =   req.session.passport.user;
+        var courseID        =   req.param('id');
+        var newCourseName   =   req.param('name');
 
-        if (!userID) {
+        console.log(userID  );
+        console.log(courseID );
+        console.log(newCourseName);
+
+        if(!userID){
+            console.log('No existe un usuario Logueado');
+            res.send('No existe un usuario Logueado');
+            return;
+        }
+
+        User.findOne({id:userID, role:'professor'}).populate('courses').exec(function findCB(err,found) {
+            if (err) {
+                res.send(err);
+                return;
+            }
+
+            if (!found.courses[0]) {
+                console.log(found);
+                console.log('La clase no existe, o bien no tiene permisos para modificarla');
+                res.send('La clase no existe, o bien no tiene permisos para modificarla');
+                return;
+            }
+
+            Course.update({id:courseID},{name:newCourseName}).exec(function afterUpdate(err,course){
+                if(err){
+                    res.send(err);
+                    return;
+                }
+                res.send('Nueva clase actualizada correctamente');
+                /*
+                if (req.isSocket){
+                    Course.publishUpdate(course.id,{name:course.name});
+                }
+                */
+            });
+
+
+
+        });
+
+
+    },
+
+    delete : function (req, res) {
+
+        var userID      =   req.session.passport.user;
+        var courseID    =   req.param('id');
+
+        if(!userID){
             console.log('No existe un usuario Logueado');
             res.send('No existe un usuario Logueado');
             return;
         }
 
 
-        Co.update({name: 'Walter Jr'}, {name: 'Flynn'}).exec(function afterwards(err, updated) {
+        User.findOne({id:userID, role:'professor'}).populate('courses',{id:courseID}).exec(function findCB(err,user) {
+            console.log(user);
 
-            if (err) {
-                // handle error here- e.g. `res.serverError(err);`
+            if(!user.courses[0]){
+                console.log('No existe el archivo. Probablemente ya haya sido eliminado');
+                res.send('No existe el archivo. Probablemente ya haya sido eliminado');
                 return;
             }
 
-            console.log('Updated user to have name ' + updated[0].name);
+            Course.destroy({id:courseID}).exec(function deleteCB(err){
+                console.log('Curso Eliminado');
+                res.send('Curso Eliminado');
+            });
 
-        });*/
+
+            if (req.isSocket){
+                Course.publishDestroy({id:courseID});
+            }
+
+        });
+
+
     }
 
 
--- assets/js/controllers/professorCourse.js
+++ assets/js/controllers/professorCourse.js
@@ -27,7 +27,9 @@ app.controller('professorCourse', ['$scope', '$rootScope', "$sailsBind", functio
         //$scope.user not updated yet
         //$scope.courses.pop();
         //return $http.post('/saveUser', data);
-        angular.extend(data, {id: id});
+        //angular.extend(data, {id: id});   //Esto te comente yo Marku, sails bind se fija si si el ''id'' ya esta asignado,
+                                            // y cree que no es un item nuevo (linea 121 de angular-sails-bind.js).
+                                            // De todas forma recibe el 'data' vacio por eso no lo manda al servidor
         $scope.courses.push(data);
         console.log(JSON.stringify(data));
     };
@@ -36,15 +38,27 @@ app.controller('professorCourse', ['$scope', '$rootScope', "$sailsBind", functio
         if (data == '') {
             return "Please fill a name";
         }
+    }
 
-/*
-    $scope.addCourseDEPRUEBA = function () {
-        io.socket.post('/api/newCourse/', {name: 'aolja'});
-    };
+    /*
+     $scope.addCourseDEPRUEBA = function () {
+     io.socket.post('/api/course', {name: '345vgv'});
+         $scope.$apply();
+     };
 
 
-    $scope.addCoursePruebaGET = function () {
-        io.socket.get('/api/myCourses/');
-    };*/
+     $scope.addCoursePruebaGET = function () {
+         io.socket.get('/api/course/');
+         $scope.$apply();
+     }
 
+    $scope.destroyCoursePUT = function () {
+        io.socket.delete('/api/course/destroy/40');
+        $scope.$apply();
+    },
+    $scope.updateCoursePUT = function () {
+        io.socket.put('/api/course/update/3?name=biologia1');
+        $scope.$apply();
+    }
+    */
 }]);
