840225c63fc9092b534e861bfb4b961dd5011e44
Ricardo Martin Marcucci <rmarku@gmail.com>
2014-10-25 22:05:51 -0300

    changed buttons and styles of buttons

--- assets/js/controllers/professorCourse.js
+++ assets/js/controllers/professorCourse.js
@@ -5,27 +5,29 @@
 
 app.controller('professorCourse', ['$scope', '$rootScope', "$sailsBind", function ($scope, $rootScope, $sailsBind) {
     $scope.courses = {};
+    $scope.loading = '';
+
     $sailsBind.bind("api/course", $scope);
+
     $scope.removeCourse = function (index) {
         $scope.courses.splice(index, 1);
     };
 // add user
     $scope.addCourse = function () {
+
         $scope.inserted = {
             name: ''
         };
         io.socket.put("/api/course/create/", $scope.inserted);
-//$scope.courses.push(c );
     };
     $scope.saveCourse = function (data, id) {
         angular.extend(data, {id: id});
         $scope.courses.push(data);
-        console.log(JSON.stringify(data));
     };
     $scope.checkName = function (data) {
-        console.log(data);
         if (data == '') {
             return "Please fill a name";
         }
     };
+    $scope.loading = 'hidden';
 }]);
