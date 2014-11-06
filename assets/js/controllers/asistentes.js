fcd071d33b2bff59d82214f727cf7117f7fe34ea
fabriciocollino <fabricio.collino@gmail.com>
2014-11-06 13:01:43 -0300

    Asistencia con filtro de clase

--- assets/js/controllers/asistentes.js
+++ assets/js/controllers/asistentes.js
@@ -7,7 +7,7 @@
 app.controller("Asistentes", function ($scope, $sailsBind) {
     $scope.$parent.getLiveClassStudent().then(function (liveClassID) {
 
-        $sailsBind.bind("api/user", $scope, {"role": "student", "live_class_student": liveClassID});
+        $sailsBind.bind("api/user", $scope, {"role": "student"});
         $scope.colorFor = {
             'Online': 'success',
             'Offline': 'danger'
